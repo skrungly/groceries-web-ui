fd35d1b7079120b743ac4988894f8ea28b96ed01
kingsley mcdonald <kingsleymcdonald@outlook.com>
2026-08-23 03:11:45 +0100

    create basic routes for layout and homepage

---
+++ src/views/LayoutView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView, useRoute } from "vue-router"
+
+const route = useRoute()
+</script>
+
+<template>
+  <header>
+    <nav>
+      <!-- ... -->
+    </nav>
+  </header>
+
+  <main>
+    <RouterView v-slot="{ Component }">
+      <component :is="Component"/>
+    </RouterView>
+  </main>
+
+  <footer>
+    <!-- ... -->
+  </footer>
+</template>
