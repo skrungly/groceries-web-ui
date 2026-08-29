99fa2411c6f5cad9df8f83e1f5dd71f5759d092b
skrungly <kingsleymcdonald@outlook.com>
2026-08-29 03:54:41 +0100

    form for new items now is its own component

---
+++ src/components/NewItemForm.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+
+let barcodeInput: Ref<string | null> = ref(null)
+
+let checkedBarcode = ref(false);
+
+// /**
+//  * returns the soonest known expiry date for a given Item.
+//  *
+//  * @param {Item} item an Item
+//  */
+// function findActualExpiry(item: Item): Date | null {
+//   if (!item.opened_at || !item.product.shelf_life_opened) {
+//     return item.expires_at
+//   }
+
+//   let opened_expiry = new Date(
+//     item.opened_at.getTime() + item.product.shelf_life_opened
+//   )
+
+//   if (!item.expires_at) {
+//     return opened_expiry
+//   }
+
+//   return opened_expiry < item.expires_at ? opened_expiry : item.expires_at
+// }
+
+async function submit() {
+  checkedBarcode.value = true
+
+  barcodeInput.value = null
+}
+</script>
+
+<template>
+  <form @submit.prevent="submit" class="flex flex-col gap-2 pt-4">
+    <input type="text" v-model="barcodeInput" placeholder="scan a barcode" class="input w-full"/>
+    <button type="submit" class="hidden"></button>
+
+    <div class="divider">or</div>
+
+    <div class="cursor-not-allowed">
+      <button class="btn btn-primary btn-disabled w-full" disabled>select custom product</button>
+    </div>
+
+    <p class="text-xs text-center text-base-content/60">(coming soon)</p>
+  </form>
+</template>
--- src/views/ItemListView.vue
+++ src/views/ItemListView.vue
@@ -1,55 +1,26 @@
 <script setup lang="ts">
 import { onMounted, ref, type Ref } from 'vue'
 
-import { api, type Item, type Product } from '@/api'
-import Modal from '@/components/Modal.vue'
+import { api, type Item } from '@/api'
 import { time_ago } from '@/utils'
+import NewItemForm from '@/components/NewItemForm.vue'
+import Modal from '@/components/Modal.vue'
 
 let showNewItemModal = ref(false)
-
-// /**
-//  * returns the soonest known expiry date for a given Item.
-//  *
-//  * @param {Item} item an Item
-//  */
-// function actualExpiry(item: Item): Date | null {
-//   if (!item.opened_at || !item.product.shelf_life_opened) {
-//     return item.expires_at
-//   }
-
-//   let opened_expiry = new Date(
-//     item.opened_at.getTime() + item.product.shelf_life_opened
-//   )
-
-//   if (!item.expires_at) {
-//     return opened_expiry
-//   }
-
-//   return opened_expiry < item.expires_at ? opened_expiry : item.expires_at
-// }
-
-let barcodeInput: Ref<string | null> = ref(null)
-// let newItem: Ref<Item | null> = ref(null)
 
 /* just a couple of functions for debugging purposes */
 function showItem(item: Item) {
   return alert(item.product.name)
 }
 
-function checkNewItem() {
-  showNewItemModal.value = false
-  console.log(barcodeInput.value)
-}
-
 let items: Ref<Item[] | null> = ref(null)
-let shownItems: Ref<Item[] | null> = ref(null)
 
 onMounted(async () => {
   // TODO: error handling
-  items.value = await api.get<Item[]>("items")
-    .then(response => response.data)
-
-  shownItems.value = items.value?.filter(i => i.percent_remaining)!
+  items.value = (
+    await api.get<Item[]>("/items")
+      .then(response => response.data)
+  ).filter(i => i.percent_remaining)!
 })
 </script>
 
@@ -71,7 +42,7 @@ onMounted(async () => {
         </thead>
 
         <tbody>
-          <tr v-if="shownItems" v-for="item in shownItems" class="hover:bg-base-200 duration-100 cursor-pointer" @click="showItem(item)">
+          <tr v-if="items" v-for="item in items" class="hover:bg-base-200 duration-100 cursor-pointer" @click="showItem(item)">
             <!-- TODO: ItemTableRow component to allow storing each actualExpiry result -->
             <td><progress class="progress w-full" :value="item.percent_remaining" max="100"></progress></td>
             <td class="text-nowrap max-w-0 overflow-hidden text-ellipsis">{{ item.product.name }}</td>
@@ -89,19 +60,7 @@ onMounted(async () => {
     <Modal v-show="showNewItemModal" @close="showNewItemModal = false">
       <h3 class="text-lg font-bold">new item</h3>
 
-      <form @submit.prevent="checkNewItem" class="flex flex-col gap-2 pt-4">
-        <div class="join">
-          <input type="text" v-model="barcodeInput" placeholder="scan a barcode" class="input grow join-item"/>
-          <button type="submit" class="btn btn-primary join-item">+</button>
-        </div>
-
-        <div class="divider">or</div>
-
-        <div class="cursor-not-allowed">
-          <button class="btn btn-primary btn-disabled w-full" disabled>select custom product</button>
-        </div>
-        <p class="text-xs text-center text-base-content/60">(coming soon)</p>
-      </form>
+      <NewItemForm></NewItemForm>
     </Modal>
   </div>
 </template>
