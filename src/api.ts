2a37a56c3e5124e8d0dc9bd1a6b1170ce9e4df48
skrungly <kingsleymcdonald@outlook.com>
2026-08-29 02:30:31 +0100

    install axios and set up basic model interfaces

---
+++ src/api.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 2000,
+})
+
+export interface Timestamped {
+  updated_at: string
+  created_at: string
+}
+
+// note that properties are nullable but not optional.
+// these accurately reflect the api's json responses
+export interface Product extends Timestamped {
+  id: string
+  user_id: string | null
+  name: string
+  barcode: string | null
+  shelf_life_opened: number | null
+  quantity: number
+  cost: number
+  net_weight: number
+}
+
+export interface Item extends Timestamped {
+  id: string
+  user_id: string | null
+  product: Product
+  percent_remaining: number
+  percent_wasted: number
+  expires_at: string | null
+  opened_at: string | null
+}
