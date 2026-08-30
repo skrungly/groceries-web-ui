735987dc51883544e15afed9a2c56e79ca8c5122
kingsley mcdonald <kingsleymcdonald@outlook.com>
2026-08-30 18:13:22 +0100

    represent wasted item amounts on item list

--- src/components/ItemListRow.vue
+++ src/components/ItemListRow.vue
@@ -17,8 +17,6 @@ function getSoonestExpiry(): Date | null {
     openedDate = new Date(Date.parse(props.item.opened_at))
   }
 
-  console.log(props.item.expires_at)
-
   if (openedDate && props.item.product.shelf_life_opened) {
     let openedExpiry = new Date(openedDate)
     openedExpiry.setDate(
@@ -38,8 +36,19 @@ const soonestExpiry = computed(getSoonestExpiry)
 </script>
 
 <template>
-  <td><progress class="progress w-full" :value="item.percent_remaining" max="100"></progress></td>
+  <td>
+    <progress
+      class="progress w-full"
+      :class="item.percent_wasted ? 'progress-error' : 'progress-neutral'"
+      :value="item.percent_remaining || item.percent_wasted"
+      :max="100 * item.product.quantity"
+    ></progress>
+  </td>
   <td class="text-nowrap max-w-0 overflow-hidden text-ellipsis">{{ item.product.name }}</td>
-  <td class="text-nowrap">{{ soonestExpiry ? time_ago.format(soonestExpiry) : null }}</td>
+  <td class="text-nowrap">
+    <span v-if="soonestExpiry && item.percent_remaining">
+      {{ time_ago.format(soonestExpiry) }}
+    </span>
+  </td>
   <td class="text-nowrap">{{ time_ago.format(new Date(Date.parse(item.created_at))) }}</td>
 </template>
